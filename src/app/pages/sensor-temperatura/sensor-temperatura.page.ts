eee90d62b311dcf7fd185df59342d945d5380f11
rendonf <43002950+rendonf@users.noreply.github.com>
2020-04-07 20:24:07 -0500

    Update temperatura.page.ts
    Solo tiene la grafica de lineas, no consume mocky

--- src/app/pages/sensor-temperatura/sensor-temperatura.page.ts
+++ src/app/pages/sensor-temperatura/sensor-temperatura.page.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef} from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-sensor-temperatura',
   templateUrl: './sensor-temperatura.page.html',
   styleUrls: ['./sensor-temperatura.page.scss'],
 })
-export class SensorTemperaturaPage implements OnInit {
+export class SensorTemperaturaPage implements OnInit{
 
-  constructor() { }
+  bars: Chart;
+  constructor(private barChart: ElementRef) { }
 
-  ngOnInit() {
+  ngOnInit () {
+    this.createBarChart();
   }
 
+  createBarChart() {
+    this.bars = new Chart( this.barChart.nativeElement.querySelector('#lineCanvas'), {
+      type: 'line',
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8'],
+        datasets: [{
+          label: 'Temperatura estandar del dia',
+          data: [35, 23, 21, 39, 30, 25, 26, 24],
+          backgroundColor: 'rgb(0, 0, 0)', // color area
+          borderColor: 'rgb(255, 121, 34)',// color linea
+          borderWidth: 1,
+          fill: false
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
 }
