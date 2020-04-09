49e452bb987088530657d82d6380dd4674d49422
FNandol <62313335+FNandol@users.noreply.github.com>
2020-04-09 17:26:52 -0500

    Actualizacion Reportes
    se agrego un formulario y una tabla en el componente de reportes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Platform, AlertController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -10,9 +9,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
-
-  
-
   public selectedIndex = 0;
   public appPages = [
     {
@@ -45,8 +41,7 @@ export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    private alertCtrl: AlertController
+    private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
@@ -57,25 +52,7 @@ export class AppComponent implements OnInit {
       this.splashScreen.show();
     });
   }
-  async presentConfirm(item) {
-    if(item.title=="Reportes"){
-      const alert = await  this.alertCtrl.create({
-        header: 'Atención',
-        message: 'Aun no se ha registrado ningun reporte, joven vuelva mas tarde' ,
-        buttons: [
-          {
-            text: 'Aceptar',
-            role: 'cancel',
-            handler: () => {
-              // console.log('Cancel clicked'); aqui se coloca lo que va a hacer el boton al presionarse
-            }
-          }
-        ]
-      });
-      alert.present();
-    }
-    
-  }
+  
   
 
   ngOnInit() {
