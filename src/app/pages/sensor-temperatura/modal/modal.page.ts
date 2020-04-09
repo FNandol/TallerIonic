640ec1975c0b081012a2e1cd69041e99aa961741
FNandol <62313335+FNandol@users.noreply.github.com>
2020-04-08 19:20:25 -0500

    Actualizacion Dialogo Modal
    Actualizacion del dialogo modal para que hiciera los cambios en la grafica al seleccionar un boton de algun  mes

--- src/app/pages/sensor-temperatura/modal/modal.page.ts
+++ src/app/pages/sensor-temperatura/modal/modal.page.ts
@@ -18,12 +18,12 @@ export class ModalPage implements OnInit {
   cambiarTemp(tempMes){
     //this.modalCtrl.dismiss();
     if (tempMes == 2){
-      this.modalCtrl.dismiss({temp: 1});  // Cierra la ventana Dialog Modal y envia un argumento
+      this.modalCtrl.dismiss( 1);  // Cierra la ventana Dialog Modal y envia un argumento
     } else { 
       if (tempMes == 3){
-        this.modalCtrl.dismiss({temp: 2});
+        this.modalCtrl.dismiss(2);
     } else { 
-      this.modalCtrl.dismiss({temp: 3});
+      this.modalCtrl.dismiss(3);
     }}
   }
 }
--- src/app/pages/sensor-temperatura/sensor-temperatura.page.ts
+++ src/app/pages/sensor-temperatura/sensor-temperatura.page.ts
@@ -21,28 +21,34 @@ export class SensorTemperaturaPage implements OnInit{
   }
 
   //------------------------------------ seleccion de la temperatura del mes ----------------------------
-   /*tempMes(n: Number){
-    let mes: Number[];
+   tempMes(n: Number){
+    let mes: any[];
     if (n == 1){
+      console.log("Hola");
         mes= [29, 23, 19, 22, 23, 25, 18, 15];
-        this.grafica.data.datasets.;
+        this.grafica.data.datasets[0].data=mes;
     } else { 
       if (n == 2){
+        console.log("Hola2");
         mes= [20, 23, 21, 29, 30, 35, 32, 33.5];
-    } else { 
+        this.grafica.data.datasets[0].data=mes;
+    } else {
+      console.log("Hola3"); 
         mes= [39, 35, 31.5, 29, 35, 37, 36, 34];
+        this.grafica.data.datasets[0].data=mes;
     }}
-    return mes;
-  }*/
+    this.grafica.update();
+  }
 
 
   // ---------------------------------------------- Grafica ----------------------------------------------
   createBarChart() {
     this.grafica = new Chart( this.lineChart.nativeElement.querySelector('#lineCanvas'), {
       type: 'line',   //tipo de grafica
-      data: {
+      data:{ 
         labels: ['1', '2', '3', '4', '5', '6', '7', '8'],   // eje x
-        datasets: [{
+        datasets: [
+          {
           label: 'Temperatura estandar del dia',  // Definicion del dato registrado
           data: [29, 23, 19, 22, 23, 25, 18, 15], //this.tempMes.(n),  // Valores
           backgroundColor: 'rgb(255, 121, 34)', // color area, o puntos valor
@@ -73,7 +79,7 @@ export class SensorTemperaturaPage implements OnInit{
 
     const { data } = await modal.onDidDismiss(); // Llega el argumento de modal
     console.log(data);
-
+    this.tempMes(data);
     //------------------------------
     //this.createBarChart(data);
 
